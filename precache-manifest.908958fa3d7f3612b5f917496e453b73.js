b4f6596b4ea6cb104082a692938b3fe91a0d9230
Angel Rodriguez <erodriguezfeliz@gmail.com>
2019-01-30 00:17:00 -0400

    Updates

---
+++ precache-manifest.908958fa3d7f3612b5f917496e453b73.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab4f8ab6e42969fd8ee0",
+    "url": "/fe_test/static/css/main.d8d971a2.chunk.css"
+  },
+  {
+    "revision": "ab4f8ab6e42969fd8ee0",
+    "url": "/fe_test/static/js/main.ab4f8ab6.chunk.js"
+  },
+  {
+    "revision": "a1cebe587b38dd962f39",
+    "url": "/fe_test/static/js/1.a1cebe58.chunk.js"
+  },
+  {
+    "revision": "db57b52b760de0526a12",
+    "url": "/fe_test/static/js/runtime~main.db57b52b.js"
+  },
+  {
+    "revision": "1960c5896d5026f4f1de8a7419c83e5d",
+    "url": "/fe_test/static/media/face1.1960c589.png"
+  },
+  {
+    "revision": "4ee040f5b66ae7fb741b805d27e885dd",
+    "url": "/fe_test/static/media/face2.4ee040f5.png"
+  },
+  {
+    "revision": "f4b37955e465a4ca29481b3fb4a15b55",
+    "url": "/fe_test/static/media/face3.f4b37955.png"
+  },
+  {
+    "revision": "4bd213e037ca7cc71a348c0abed909a3",
+    "url": "/fe_test/static/media/face4.4bd213e0.png"
+  },
+  {
+    "revision": "d339e794766ffa4f083027062b3e4de2",
+    "url": "/fe_test/static/media/face5.d339e794.png"
+  },
+  {
+    "revision": "2192a356688e3cb1c3ff7f44f0b17627",
+    "url": "/fe_test/static/media/face6.2192a356.png"
+  },
+  {
+    "revision": "93d0b2d41a435d0c9e9f8059e08d9e7e",
+    "url": "/fe_test/static/media/face7.93d0b2d4.png"
+  },
+  {
+    "revision": "053c4c52ba951d12eba8f9b1676a67b6",
+    "url": "/fe_test/static/media/splash_paint_black.053c4c52.png"
+  },
+  {
+    "revision": "f50ea6ebd9aa7e1521545bec2d43ea73",
+    "url": "/fe_test/static/media/banner_blu_ocean.f50ea6eb.png"
+  },
+  {
+    "revision": "1f65235ded6ecd7942e5a0ab37a0c0f4",
+    "url": "/fe_test/static/media/banner_corrugated.1f65235d.png"
+  },
+  {
+    "revision": "b0e0e0b4717e99ef4c07657a16ac4536",
+    "url": "/fe_test/static/media/black_small_spray.b0e0e0b4.png"
+  },
+  {
+    "revision": "3f113b8d7ccca1e1cc92d67bf7921e08",
+    "url": "/fe_test/index.html"
+  }
+];
